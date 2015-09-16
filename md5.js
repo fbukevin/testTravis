94b7e07328d3ed796f2ef1e6cf987ae76c212f5b
fbukevin <fbukevin@gmail.com>
2015-09-16 10:15:11 +0800

    deprecate init test

--- md5.js
+++
@@ -1,8 +0,0 @@
-(function(root, undefined) {
-  // ...
-  if(typeof(module) != 'undefined') {
-    module.exports = md5;
-  } else if(root) {
-    root.md5 = md5;
-  }
-}(this));
--- test.js
+++
@@ -1,5 +0,0 @@
-describe('less than 64 bytes', function() {
-  it('should be successful', function() {
-    expect(md5('')).to.be('d41d8cd98f00b204e9800998ecf8427e');
-  });
-});
